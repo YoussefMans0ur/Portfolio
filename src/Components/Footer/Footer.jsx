bf3ca4ba9ef39f1585f7568e0238048d6f62bed1
YoussefMans0ur <youssefmansourr8111@gmail.com>
2024-11-23 16:17:16 +0200

    edit footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (<>
-    <footer className='py-3 text-center text-white bg-black'>Copy Right 2024 © By <span className='fw-bold'>Youssef Mansour</span> Rights Reserved</footer>
+    <footer className='py-3 text-center text-white bg-black px-3 small'>Copy Right 2024 © By <span className='fw-bold'>Youssef Mansour</span> Rights Reserved</footer>
   </>)
 }
